36ae1b7e1a8f97dd1bc8dde21a127a165df23fc2
Jan Valentik <jvalentik@sk.ibm.com>
2020-03-13 10:33:02 +0100

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jan Valentik on 13/03/2020.
+//  Copyright © 2020 Cloudis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
