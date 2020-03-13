89d01892d5403676af8d3a423120870100c6a270
Jan Valentik <jvalentik@sk.ibm.com>
2020-03-13 11:16:51 +0100

    chore(*): initial commit

--- I Am PoorUITests/I_Am_PoorUITests.swift
+++ I Am PoorUITests/I_Am_PoorUITests.swift
@@ -28,6 +28,7 @@ class I_Am_PoorUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
+
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
